4ab96e3835c8fc2f4a3d5e31c9eb6b5bba7f50e4
trphan066 <trieunphan206@gmail.com>
2025-03-30 23:18:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Ví dụ: Thêm hiệu ứng cuộn mượt khi nhấp vào link
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
